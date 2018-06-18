412e18d134de55384f6a07ecb423ef175dfbffc6
dkgecov <daniellgetsov@gmail.com>
2018-06-18 10:56:27 +0300

    uploading project

--- src/main/java/bar/services/ReportManager.java
+++ src/main/java/bar/services/ReportManager.java
@@ -1,12 +1,16 @@
 package bar.services;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import bar.dao.OrderDAO;
 
@@ -27,12 +31,20 @@ public class ReportManager {
 	}
 	
 	
-	@GET
+	/*@GET
 	@Path("between")
 	@Produces("application/json")
-	public float getWeeklyProfit(Date begDate,Date endDate){
-		return orderDAO.estimateProfitBetweenTwoDates(begDate,endDate);
-	}
+	public float estimateProfitBetweenTwoDates( List<Date> Dates ){
+		return orderDAO.estimateProfitBetweenTwoDates(Dates.get(0),Dates.get(1));
+		
+	}*/
 	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String estimateProfitBetweenTwoDates( String dates){
+		return "trtretr!";
+		
+	}
 
 }
